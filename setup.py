b83e76de25f7b4afcafee8d3059f8384667ffbed
yatakeeriravi <yatakeeri.ravikiran>
2026-01-02 15:24:15 +0530

    added cookie cutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Ml project to predict emotion from text',
+    author='ravi',
+    license='',
+)
